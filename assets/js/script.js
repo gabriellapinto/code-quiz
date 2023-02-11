3c38f42f240fc48c87909f3b23de8cd17cd75f2d
Gabby <gabbympinto@gmail.com>
2023-02-11 16:15:05 -0500

    added header with high score link and timer countdown

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var timeCounter = document.querySelector(".time");
+
+var secondsLeft = 75;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeCounter.textContent = "Time: " + secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            timeDone();
+        }
+
+
+    }, 1500);
+}
+
+function timeDone() {
+    timeCounter.textContent = "TIME DONE";
+}
+
+setTime();
