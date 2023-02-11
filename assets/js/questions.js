ea50aeeb2e73514197622282a9ce5965699f9f17
Gabby <gabbympinto@gmail.com>
2023-02-11 16:31:43 -0500

    added javascript questions and added a bit of css to header

---
+++ assets/js/questions.js
@@ -0,0 +1,25 @@
+var question1 = {
+    text: "Commonly used data types DO NOT include:",
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"]
+}
+
+var question2 = {
+    text: "The condition in an if / else statement are enclosed with ______.",
+    options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"]
+}
+
+var question3 = {
+    text: "Arrays in JavaScript can be used to store ______.",
+    options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"]
+}
+
+var question4 = {
+    text: "String values must be enclosed within ______ when being assigned to variables.",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"]
+}
+
+var question5 = {
+    text: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"]
+}
+
