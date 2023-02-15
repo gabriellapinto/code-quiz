08bcb5957efba0c410dff5f7ba739d7751455b29
Gabby <gabbympinto@gmail.com>
2023-02-15 12:02:16 -0500

    added click function to start button to start the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,41 @@
 // -- TIMER CODE -- //
 var timeCounter = document.querySelector(".time");
+var questionsBox = document.querySelector("#questions-box");
+var selectionsBox = document.querySelector("#selections-box");
 var secondsLeft = 75;
 
-function setTime() {
+function timer() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timeCounter.textContent = "Time: " + secondsLeft;
-
-        if (secondsLeft >= 0) {
-
-        }
 
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
             timeDone();
         }
 
-
-    }, 1500);
-} 
+    }, 1000);
+}
 
 function timeDone() {
     timeCounter.textContent = "Sorry, you're out of time!";
 }
-
-setTime();
 // -- END TIMER CODE -- //
 
 // -- START LANDING PAGE CODE -- //
-var questionsBox = document.querySelector("#questions-box");
-var selectionsBox = document.querySelector("#selections-box");
 
 questionsBox.textContent = "Coding Quiz Challenge!";
 selectionsBox.textContent = "Try to answer the following code related questions within the time limit. Incorrect answers will penalize your score and time!";
 
+
+
+// -- RUN QUIZ FUNCTION -- //
+
+function runQuiz() {
+    timer();
+
+
+
+}
 
 
