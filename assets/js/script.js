81d1d55b69a27f82281b816eb40da42e4afde3e8
Gabby <gabbympinto@gmail.com>
2023-02-15 20:45:25 -0500

    added js to questions and options

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,14 +34,39 @@ selectionsBox.textContent = "Try to answer the following code related questions 
 function runQuiz() {
     timer();
 
+}
+
+// START HERE 
+
+function changeQuestions(id) {
+    var question = document.getElementById("#questions-box");
+
+    // Getting the question
+    question.innerText = questions[id].text;
+
+    // Getting the options
+    const op1 = document.getElementById('op1');
+    const op2 = document.getElementById('op2');
+    const op3 = document.getElementById('op3');
+    const op4 = document.getElementById('op4');
+
+    // Providing option text
+    op1.innerText = questions[id].options[0].text;
+    op2.innerText = questions[id].options[1].text;
+    op3.innerText = questions[id].options[2].text;
+    op4.innerText = questions[id].options[3].text;
+
+    // Providing true/false value to options
+    op1.value = questions[id].options[0].isCorrect;
+    op2.value = questions[id].options[1].isCorrect;
+    op3.value = questions[id].options[2].isCorrect;
+    op4.value = questions[id].options[3].isCorrect;
+
+
 
 
 }
 
 
-
-function changeQuestions() {
-
-}
 
 
