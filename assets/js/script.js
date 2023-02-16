dfa5dfff5720a02c01d727d36e998f0aad09b05e
Gabby <gabbympinto@gmail.com>
2023-02-15 21:23:40 -0500

    added js to hide values after start button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var timeCounter = document.querySelector(".time");
 var questionsBox = document.querySelector("#questions-box");
 var selectionsBox = document.querySelector("#selections-box");
 var correctAnswerBox = document.querySelector("#wrong-right-box");
+var startBtn = document.querySelector("#start-btn");
 var secondsLeft = 75;
 var currentQuestion = 0;
 
@@ -14,6 +15,10 @@ function timer() {
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
             timeDone();
+        }
+        // Deducts 10 seconds if user answers incorrectly
+        if (isCorrect = false) {
+            secondsLeft = secondsLeft - 10;
         }
 
     }, 1000);
@@ -32,17 +37,23 @@ selectionsBox.textContent = "Try to answer the following code related questions 
 
 // The runQuiz function is called when the start button is clicked
 function runQuiz() {
+    // Hides start button after it is clicked
+    startBtn.style.display = "none";
     timer();
+    changeQuestions();
 
 }
 
 // START HERE 
 
 function changeQuestions(id) {
-    var question = document.getElementById("#questions-box");
+    // Emptys both the question and answers container
+    selectionsBox.textContent = "";
+    questionsBox.textContent = "",
+
 
     // Getting the question
-    question.innerText = questions[id].text;
+    questionsBox.textContent = questions[id].text;
 
     // Getting the options
     const op1 = document.getElementById('op1');
