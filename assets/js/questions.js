80f4d94324fab5a32a519aff3812bcd71c17de15
Gabby <gabbympinto@gmail.com>
2023-02-20 18:53:59 -0500

    reformatted questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,40 +1,25 @@
 var questions = [{
     text: "Commonly used data types DO NOT include:",
-    options: [{text: "1. strings", isCorrect: false}, 
-              {text: "2. booleans", isCorrect: false},
-              {text: "3. alerts", isCorrect: true},
-              {text: "4. numbers", isCorrect: false}
-    ]
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    correctAnswer: "3. alerts",
 },
 {
     text: "The condition in an if / else statement are enclosed with ______.",
-    options: [{text: "1. quotes", isCorrect: false},
-              {text: "2. curly brackets", isCorrect: false},
-              {text: "3. parenthesis", isCorrect: true},
-              {text: "4. square brackets", isCorrect: false}
-    ]
+    options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    correctAnswer: "3. parenthesis",
 },
 {
     text: "Arrays in JavaScript can be used to store ______.",
-    options: [{text: "1. numbers and strings", isCorrect: false},
-              {text: "2. other arrays", isCorrect: false}, 
-              {text: "3. booleans", isCorrect: false},
-              {text: "4. all of the above", isCorrect: true}
-    ]
+    options: ["1. numbers and strings", "2. other arrays","3. booleans", "4. all of the above"],
+    correctAnswer: "4. all of the above",
 },
 {
     text: "String values must be enclosed within ______ when being assigned to variables.",
-    options: [{text: "1. commas", isCorrect: false},
-              {text: "2. curly brackets", isCorrect: false},
-              {text: "3. quotes", isCorrect: true}, 
-              {text: "4. parenthesis", isCorrect: false}
-    ]
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    correctAnswer: "3. quotes",
 },
 {
     text: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: [{text: "1. JavaScript", isCorrect: false},
-              {text: "2. terminal/bash", isCorrect: false},
-              {text: "3. for loops", isCorrect: false},
-              {text: "4. console.log", isCorrect: true}
-    ]
+    options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    correctAnswer: "4. console.log",
 }]
